f10e5bba0bc739e84b1c533fd4ecb7dbea854969
Daniel Armak <danarmak@gmail.com>
2014-06-07 16:04:27 +0300

    Add cancellation support to ByteSource constructors
    ByteSources built from streams and channels now also support closing the
    stream/channel when canceled.

--- bytestreams/src/main/scala/com/fsist/stream/ByteSource.scala
+++ bytestreams/src/main/scala/com/fsist/stream/ByteSource.scala
@@ -1,8 +1,8 @@
 package com.fsist.stream
 
 import akka.util.ByteString
-import com.fsist.util.{Nio, CancelToken}
-import java.io.{EOFException, InputStream}
+import com.fsist.util.{CanceledException, Nio, CancelToken}
+import java.io.{IOException, EOFException, InputStream}
 import scala.Some
 import scala.concurrent._
 import java.nio.channels.AsynchronousByteChannel
@@ -16,29 +16,60 @@ object ByteSource {
     *
     * WARNING: an InputStream is inherently blocking, and this consumes a thread for each stream wrapped in this way,
     * which is inefficient and does NOT scale. Avoid this method unless you absolutely have to wrap a legacy InputStream.
+    *
+    * @param closeOnDone If true, the stream's `close` method is called when the stream returns EOF or reading returns
+    *                    an error. More importantly, it will also be called if this Source is cancelled.
     */
-  def fromStreamAvoidThis(stream: InputStream, bufferSize: Int = 1024*16)
-                         (implicit ecc: ExecutionContext, cancel: CancelToken = CancelToken.none) : Source[ByteString] = new SourceImpl[ByteString] {
+  def fromStreamAvoidThis(stream: InputStream, bufferSize: Int = 1024 * 16, closeOnDone: Boolean = true)
+                         (implicit ecc: ExecutionContext, cancel: CancelToken = CancelToken.none): Source[ByteString] = new SourceImpl[ByteString] {
     override def cancelToken: CancelToken = cancel
+
     override def ec: ExecutionContext = ecc
+
+    if (closeOnDone) {
+      try {
+        cancelToken.future.map(_ => stream.close())
+      }
+      catch {
+        case e: IOException =>
+      }
+    }
 
     private val buffer = new Array[Byte](bufferSize)
 
     override protected def produce(): Future[Option[ByteString]] = Future {
       blocking {
-        stream.read(buffer) match {
-          case -1 =>
-            stream.close()
-            None
-          case count => Some(ByteString(buffer).slice(0, count))
+        try {
+          stream.read(buffer) match {
+            case -1 =>
+              if (closeOnDone) stream.close()
+              None
+            case count => Some(ByteString(buffer).slice(0, count))
+          }
+        }
+        catch {
+          case e: IOException if cancelToken.isCanceled => throw new CanceledException()
         }
       }
     }
   }
 
-  /** An async reader from this channel represented as a Source. */
-  def apply(channel: AsynchronousByteChannel, readBufferSize: Int = 1024 * 16)
-            (implicit ec: ExecutionContext): Source[ByteString] = {
+  /** An async reader from this channel represented as a Source.
+    *
+    * @param closeOnDone If true, the channel's `close` method is called when the stream returns EOF or reading returns
+    *                    an error. More importantly, it will also be called if this Source is cancelled.
+    */
+  def apply(channel: AsynchronousByteChannel, readBufferSize: Int = 1024 * 16, closeOnDone: Boolean = true)
+           (implicit ec: ExecutionContext, cancelToken: CancelToken = CancelToken.none): Source[ByteString] = {
+    if (closeOnDone) {
+      try {
+        cancelToken.future.map(_ => channel.close())
+      }
+      catch {
+        case e: IOException =>
+      }
+    }
+
     val buf = ByteBuffer.allocate(readBufferSize)
     Source.generateM[ByteString] {
       Nio.readSome(channel, buf) map { count =>
@@ -49,7 +80,10 @@ object ByteSource {
           buf.position(0)
           Some(data)
         }
-      } recover { case e: EOFException => None}
+      } recover {
+        case e: EOFException => None
+        case e: IOException if cancelToken.isCanceled => throw new CanceledException()
+      }
     }
   }
 }
