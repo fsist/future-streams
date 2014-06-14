502455d69f78af0b9d49f25abe5d283fd4877b6e
Daniel Armak <danarmak@gmail.com>
2014-06-14 13:40:41 +0300

    More efficient implementation of NeverFuture
    Credit to Viktor Klang.

--- streams/src/main/scala/com/fsist/util/CancelToken.scala
+++ streams/src/main/scala/com/fsist/util/CancelToken.scala
@@ -2,6 +2,7 @@ package com.fsist.util
 
 import scala.concurrent._
 import scala.concurrent.duration.{FiniteDuration, Duration}
+import scala.reflect.ClassTag
 import scala.util.Try
 
 /** Used to cooperatively, asynchronously cancel operations.
@@ -13,7 +14,9 @@ import scala.util.Try
   *
   * Create instances using the companion object `apply`.
   */
-class CancelToken private (val promise: Promise[Unit], allowCancel: Boolean = true) {
+class CancelToken private(val promise: Promise[Unit], allowCancel: Boolean = true) {
+  import CancelToken._
+
   /** @return true iff the token is already canceled. */
   def isCanceled: Boolean = promise.isCompleted
 
@@ -25,7 +28,7 @@ class CancelToken private (val promise: Promise[Unit], allowCancel: Boolean = tr
     *   someFuture map (cancelToken.throwIfCanceled) map myContinuation
     * }}}
     */
-  def throwIfCanceled[T](): T=>T = t => if (isCanceled) throw new CanceledException() else t
+  def throwIfCanceled[T](): T => T = t => if (isCanceled) throw new CanceledException() else t
 
   /** Cancel this token.
     * @return true if we canceled it; false if it was already canceled.
@@ -34,11 +37,13 @@ class CancelToken private (val promise: Promise[Unit], allowCancel: Boolean = tr
   def cancel(): Boolean = if (allowCancel) promise.trySuccess(()) else throw new IllegalArgumentException("This token doens't allow cancellation")
 
   /** Returns a future that is completed when the token is canceled (it may be already completed when it's returned). */
-  def future: Future[Unit] = if (allowCancel) promise.future else NeverFuture
+  def future: Future[Unit] = if (allowCancel) promise.future else never
 
   /** Returns a future that is failed with a CanceledException when the token is canceled (it may be already completed when it's returned). */
   def futureFail(implicit ec: ExecutionContext): Future[Unit] =
-    future flatMap (_ => Future.failed(new CanceledException()))
+    if (allowCancel) {
+      future flatMap (_ => Future.failed(new CanceledException()))
+    } else never
 
   /** The returned future does one of two things:
     *
@@ -51,8 +56,11 @@ class CancelToken private (val promise: Promise[Unit], allowCancel: Boolean = tr
     *
     * Whenever possible, use or implement an actually cancelable future instead of using this method.
     */
-  def abandonOnCancel[T](other: Future[T])(implicit ec: ExecutionContext) : Future[T] =
-    Future.firstCompletedOf(Seq(futureFail.map(_.asInstanceOf[T]), other))
+  def abandonOnCancel[T](other: Future[T])(implicit ec: ExecutionContext): Future[T] =
+    if (allowCancel) {
+      Future.firstCompletedOf(Seq(futureFail.map(_.asInstanceOf[T]), other))
+    }
+    else other
 }
 
 object CancelToken {
@@ -60,25 +68,57 @@ object CancelToken {
 
   /** A singleton token that can never be cancelled. */
   val none: CancelToken = new CancelToken(Promise[Unit](), false)
+
+  private val never : Future[Unit] = new NeverFuture()
 }
 
 /** Thrown by methods of CancelToken as documented when an operation has been canceled. */
 class CanceledException(msg: String = "Token canceled") extends Exception(msg)
 
 /** Represents a Future that never completes. This allows us to discard continuation functions, which would otherwise
-  * stay forever in memory (for CancelToken.none). */
-object NeverFuture extends Future[Unit] {
-  private val promise = Promise[Unit]()
-
-  override def onComplete[U](func: (Try[Unit]) => U)(implicit executor: ExecutionContext): Unit = ()
+  * stay forever in memory (for CancelToken.none).
+  *
+  * Credited to Viktor Klang. Cf also https://github.com/viktorklang/scala/commit/0f91e9050e1b5fa7a9aecfd973f85f83d6d6b0d2
+  */
+class NeverFuture() extends Future[Nothing] {
+  type T = Nothing
+  override def onSuccess[U](pf: PartialFunction[T, U])(implicit executor: ExecutionContext): Unit = ()
+  override def onFailure[U](pf: PartialFunction[Throwable, U])(implicit executor: ExecutionContext): Unit = ()
+  override def onComplete[U](f: Try[T] => U)(implicit executor: ExecutionContext): Unit = ()
   override def isCompleted: Boolean = false
-  override val value: Option[Try[Unit]] = None
-  
-  // Delegate to a real Promise's implementation
+  override def value: Option[Try[T]] = None
+  override val failed: Future[Throwable] = this //new NeverFuture[Throwable]
+  override def foreach[U](f: T => U)(implicit executor: ExecutionContext): Unit = ()
+  override def transform[S](s: T => S, f: Throwable => Throwable)(implicit executor: ExecutionContext): Future[S] = this //new NeverFuture[S]
+  override def map[S](f: T => S)(implicit executor: ExecutionContext): Future[S] = this //new NeverFuture[S]
+  override def flatMap[S](f: T => Future[S])(implicit executor: ExecutionContext): Future[S] = this //new NeverFuture[S]
+  override def filter(p: T => Boolean)(implicit executor: ExecutionContext): Future[T] = this
+  override def collect[S](pf: PartialFunction[T, S])(implicit executor: ExecutionContext): Future[S] = this //new NeverFuture[S]
+  override def recover[U >: T](pf: PartialFunction[Throwable, U])(implicit executor: ExecutionContext): Future[U] = this
+  override def recoverWith[U >: T](pf: PartialFunction[Throwable, Future[U]])(implicit executor: ExecutionContext): Future[U] = this
+  override def zip[U](that: Future[U]): Future[(T, U)] = this //new NeverFuture[(T, U)]
+  override def fallbackTo[U >: T](that: Future[U]): Future[U] = this
+  override def mapTo[S](implicit tag: ClassTag[S]): Future[S] = this //new NeverFuture[S]
+  override def andThen[U](pf: PartialFunction[Try[T], U])(implicit executor: ExecutionContext): Future[T] = this
 
-  override def result(atMost: Duration)(implicit permit: CanAwait): Unit = promise.future.result(atMost)
-  override def ready(atMost: Duration)(implicit permit: CanAwait): NeverFuture.type = {
-    promise.future.ready(atMost)
-    this
+  override def result(atMost: Duration)(implicit permit: CanAwait): T = {
+    atMost match {
+      case e if e eq Duration.Undefined => throw new IllegalArgumentException("cannot wait for Undefined period")
+      case Duration.Inf =>
+        while (true) Thread.sleep(Long.MaxValue)
+        throw new TimeoutException // Never reached, but allows the code to compile
+      case f: FiniteDuration if f > Duration.Zero =>
+        val nanos = (f.toNanos - (f.toMillis * 1000000)).toInt
+        Thread.sleep(f.toMillis, nanos)
+        throw new TimeoutException
+      case _ =>
+        // For negative durations, don't wait at all
+        throw new TimeoutException
+    }
+  }
+
+  override def ready(atMost: Duration)(implicit permit: CanAwait): this.type = {
+    ready(atMost)
+    throw new TimeoutException
   }
 }
