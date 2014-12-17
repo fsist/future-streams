866f920ca93dc79bde26974dda5cda487667c522
Daniel Armak <danarmak@gmail.com>
2014-12-17 11:16:57 +0200

    Removed package docs, they are outdated and the readme replaces them

--- src/main/scala/com/fsist/stream/package.scala
+++ src/main/scala/com/fsist/stream/package.scala
@@ -1,24 +1,9 @@
 package com.fsist
 
+import akka.http.util.FastFuture
+
 import scala.concurrent.Future
 
-/** Overview of types involved in different stages:
-  *
-  * == Model ==
-  *
-  * StreamComponent: base trait of everything linked to a StreamBuilder
-  * Source: producer. Implementations don't provide a single typed function `produce` to allow specializations to
-  *                   avoid the overhead of wrapping every element in Option to indicate EOF.
-  * Sink: consumer. Implementations provide the onNext/onComplete/onError triplet.
-  * Connector: has >=1 sources and >=1 sinks and routing logic.
-  * Transform: a generalized source -> source mapping.
-  *
-  * == Internal representation in the Builder during modeling ==
-  *
-  * Every StreamComponent is register()ed and becomes a graph node: this is every Source, Sink, Transform and Connector.
-  * Every link is registered and becomes a graph edge. Graph edge origins are Sources, Transforms and Connectors.
-  * Edge destinations are Sinks, Transforms and Connectors.
-  */
 package object stream {
-  val futureSuccess = Future.successful(())
+  val futureSuccess : Future[Unit] = FastFuture.successful(())
 }
