f682ffb025a621e1145a8934e5950e0760a3b9c3
Daniel Armak <danarmak@gmail.com>
2015-05-18 13:49:43 +0300

    Add FutureStreamBuilder.runningStream, allowing access to the eventual stream from any component builder. Tag 0.9.0.

--- src/main/scala/com/fsist/stream/run/FutureStreamBuilder.scala
+++ src/main/scala/com/fsist/stream/run/FutureStreamBuilder.scala
@@ -39,11 +39,10 @@ import scala.language.implicitConversions
   * will be undefined.
   */
 class FutureStreamBuilder extends LazyLogging {
-
   private val graph = new StreamGraph
   private val linked = mutable.HashSet[FutureStreamBuilder]()
-
   private val ran = new AtomicBoolean()
+  private val runningStreamPromise = Promise[RunningStream]()
 
   // == PUBLIC API ==
 
@@ -90,132 +89,149 @@ class FutureStreamBuilder extends LazyLogging {
     }
   }
 
+  /** Returns a Future that is fulfilled once `run` has been called.
+    *
+    * This is useful because it allows the constructors of stream components to acquire the eventual stream and then
+    * link it to outside failure conditions by calling `RunningStream.fail`.
+    */
+  def runningStream: Future[RunningStream] = runningStreamPromise.future
+
   /** Builds and starts a runnable FutureStream from the current graph. */
   def run()(implicit ec: ExecutionContext): RunningStream = {
-    require(! ran.getAndSet(true), "Must not run() the same stream twice")
+    require(!ran.getAndSet(true), "Must not run() the same stream twice")
 
-    val allLinked = mutable.HashSet[FutureStreamBuilder]()
-    gatherLinked(this, allLinked)
+    try {
 
-    for (builder <- allLinked if builder ne this) {
-      require(! builder.ran.getAndSet(true), "Must not run() the same stream twice")
-      graph.mergeFrom(builder.graph)
-    }
+      val allLinked = mutable.HashSet[FutureStreamBuilder]()
+      gatherLinked(this, allLinked)
 
-    validateBeforeBuilding()
+      for (builder <- allLinked if builder ne this) {
+        require(!builder.ran.getAndSet(true), "Must not run() the same stream twice")
+        graph.mergeFrom(builder.graph)
+      }
 
-    logger.trace(s"Running stream:\n$graph")
+      validateBeforeBuilding()
 
-    // Declare here, set later, and graphOps will access it later from its lazy val
-    var stateMachinesVector: Vector[StateMachine] = Vector.empty
+      logger.trace(s"Running stream:\n$graph")
 
-    val graphOps = new GraphOps with LazyLogging {
-      private lazy val stateMachines = stateMachinesVector
-      private val failure = Promise[Throwable]()
+      // Declare here, set later, and graphOps will access it later from its lazy val
+      var stateMachinesVector: Vector[StateMachine] = Vector.empty
 
-      override def failGraph(th: Throwable): Unit = {
-        if (failure.trySuccess(th))
-          stateMachines foreach (_.fail(th))
-        else {
-          val existing = failure.future.value.get.get
-          if (th ne existing) {
-            logger.trace(s"Discarding additional error $th, already failed with $existing")
+      val graphOps = new GraphOps with LazyLogging {
+        private lazy val stateMachines = stateMachinesVector
+        private val failure = Promise[Throwable]()
+
+        override def failGraph(th: Throwable): Unit = {
+          if (failure.trySuccess(th))
+            stateMachines foreach (_.fail(th))
+          else {
+            val existing = failure.future.value.get.get
+            if (th ne existing) {
+              logger.trace(s"Discarding additional error $th, already failed with $existing")
+            }
           }
         }
       }
-    }
 
-    val allConnectors = graph.connectors()
+      val allConnectors = graph.connectors()
 
-    // Create the StateMachine instances
+      // Create the StateMachine instances
 
-    val connectorMachines: Map[ConnectorId[_], ConnectorMachine[_]] =
-      allConnectors.map({
-        case node@ConnectorId(merger: Merger[_]) => (node, new MergerMachine(merger, graphOps))
-        case node@ConnectorId(splitter: Splitter[_]) => (node, new SplitterMachine(splitter, graphOps))
-        case node@ConnectorId(scatterer: Scatterer[_]) => (node, new ScattererMachine(scatterer, graphOps))
-        case node@ConnectorId(concatenator: Concatenator[_]) => (node, new ConcatenatorMachine(concatenator, graphOps))
-      }).toMap
+      val connectorMachines: Map[ConnectorId[_], ConnectorMachine[_]] =
+        allConnectors.map({
+          case node@ConnectorId(merger: Merger[_]) => (node, new MergerMachine(merger, graphOps))
+          case node@ConnectorId(splitter: Splitter[_]) => (node, new SplitterMachine(splitter, graphOps))
+          case node@ConnectorId(scatterer: Scatterer[_]) => (node, new ScattererMachine(scatterer, graphOps))
+          case node@ConnectorId(concatenator: Concatenator[_]) => (node, new ConcatenatorMachine(concatenator, graphOps))
+        }).toMap
 
-    // All component types other than connectors
-    val componentMachines: Map[ComponentId, StateMachine] =
-      (for (node@ComponentId(component) <- graph.components.keys if !component.isInstanceOf[ConnectorEdge[_]]) yield {
-        component match {
-          case input: StreamProducer[_] => (node, new ProducerMachine(input, graphOps))
-          case input: DelayedSource[_] => (node, new DelayedSourceMachine(input, graphOps))
-          case input: DrivenSource[_] => (node, new DrivenSourceMachine(input, graphOps))
-          case output: StreamConsumer[_, _] => (node, new ConsumerMachine(output, graphOps))
-          case output: DelayedSink[_, _] => (node, new DelayedSinkMachine(output, graphOps))
-          case nop: NopTransform[_] => (node, new NopMachine(nop, graphOps))
-          case pipe: DelayedPipe[_, _] => (node, new DelayedPipeMachine(pipe, graphOps))
-          case transform: Transform[_, _] => (node, new TransformMachine(transform, graphOps))
-          case other => throw new NotImplementedError(other.toString) // Can't really happen, this is to silence the error due to StreamComponentBase not being sealed
+      // All component types other than connectors
+      val componentMachines: Map[ComponentId, StateMachine] =
+        (for (node@ComponentId(component) <- graph.components.keys if !component.isInstanceOf[ConnectorEdge[_]]) yield {
+          component match {
+            case input: StreamProducer[_] => (node, new ProducerMachine(input, graphOps))
+            case input: DelayedSource[_] => (node, new DelayedSourceMachine(input, graphOps))
+            case input: DrivenSource[_] => (node, new DrivenSourceMachine(input, graphOps))
+            case output: StreamConsumer[_, _] => (node, new ConsumerMachine(output, graphOps))
+            case output: DelayedSink[_, _] => (node, new DelayedSinkMachine(output, graphOps))
+            case nop: NopTransform[_] => (node, new NopMachine(nop, graphOps))
+            case pipe: DelayedPipe[_, _] => (node, new DelayedPipeMachine(pipe, graphOps))
+            case transform: Transform[_, _] => (node, new TransformMachine(transform, graphOps))
+            case other => throw new NotImplementedError(other.toString) // Can't really happen, this is to silence the error due to StreamComponentBase not being sealed
+          }
+        }).toMap
+
+      // All machines including connectors
+      val allMachines = componentMachines ++ {
+        val builder = Map.newBuilder[ComponentId, StateMachine]
+        // Try to write it using connectorMachines.flatMap - you'll get some delicious type errors
+        connectorMachines.foreach {
+          case (k, v) => k.value.edges.foreach {
+            case e => builder += ((e, v))
+          }
         }
-      }).toMap
-
-    // All machines including connectors
-    val allMachines = componentMachines ++ {
-      val builder = Map.newBuilder[ComponentId, StateMachine]
-      // Try to write it using connectorMachines.flatMap - you'll get some delicious type errors
-      connectorMachines.foreach {
-        case (k, v) => k.value.edges.foreach {
-          case e => builder += ((e, v))
-        }
-      }
-      builder.result()
-    }
-
-    stateMachinesVector = allMachines.values.toVector
-
-    // Only the sink-like machines
-    val sinkMachines: Map[ComponentId, ConsumerProvider[_]] =
-      allMachines.filter {
-        case (component, machine) => machine.isInstanceOf[ConsumerProvider[_]]
-      }.mapValues(_.asInstanceOf[ConsumerProvider[_]])
-
-    // Connect the state machines to one another
-    for ((source, Some(sink)) <- graph.components) {
-
-      val consumerProvider = sink.value match {
-        case connectorInput: ConnectorInput[_] => connectorMachines(connectorInput.connector) match {
-          case machine: ConnectorMachine[_] => ConsumerProvider(machine, connectorInput.index)
-        }
-        case component: StreamComponent => componentMachines(component) match {
-          case machine: ConsumerProvider[_] => machine: ConsumerProvider[_]
-
-          case _: ConnectorMachine[_] => throw new IllegalStateException("A ConnectorMachine is in the componentMachines map")
-          case _: DrivenSourceMachine[_] | _: DelayedSourceMachine[_] | _: ProducerMachine[_] => throw new IllegalStateException("Source machine appears as a Sink")
-        }
+        builder.result()
       }
 
-      source.value match {
-        case output: ConnectorOutput[_] =>
-          val machine = connectorMachines(output.connector)
-          machine.nexts(output.index) = Some(consumerProvider.asInstanceOf[ConsumerProvider[machine.TT]])
+      stateMachinesVector = allMachines.values.toVector
 
-        case component: StreamComponent =>
-          componentMachines(component) match {
-            case machine: StateMachineWithOneOutput[_] =>
-              machine.next = Some(consumerProvider.asInstanceOf[ConsumerProvider[machine.TOut]])
+      // Only the sink-like machines
+      val sinkMachines: Map[ComponentId, ConsumerProvider[_]] =
+        allMachines.filter {
+          case (component, machine) => machine.isInstanceOf[ConsumerProvider[_]]
+        }.mapValues(_.asInstanceOf[ConsumerProvider[_]])
 
-            case _: ConsumerMachine[_ , _] | _: DelayedSinkMachine[_, _] => throw new IllegalStateException("Sink machine appears as a Source")
+      // Connect the state machines to one another
+      for ((source, Some(sink)) <- graph.components) {
+
+        val consumerProvider = sink.value match {
+          case connectorInput: ConnectorInput[_] => connectorMachines(connectorInput.connector) match {
+            case machine: ConnectorMachine[_] => ConsumerProvider(machine, connectorInput.index)
+          }
+          case component: StreamComponent => componentMachines(component) match {
+            case machine: ConsumerProvider[_] => machine: ConsumerProvider[_]
+
             case _: ConnectorMachine[_] => throw new IllegalStateException("A ConnectorMachine is in the componentMachines map")
+            case _: DrivenSourceMachine[_] | _: DelayedSourceMachine[_] | _: ProducerMachine[_] => throw new IllegalStateException("Source machine appears as a Sink")
           }
-      }
-    }
+        }
 
-    // Start the initial machines. Note that `allMachines` can contain the same value many times (for Connectors mapped
-    // from multiple StreamComponents which are their edges) so we use .toSet to only `run` each machine once.
-    for (machine <- allMachines.values.toSet if machine.isInstanceOf[RunnableMachine]) {
-      Future {
-        machine.asInstanceOf[RunnableMachine].run()
-      } recover {
-        case NonFatal(e) =>
-          graphOps.failGraph(e)
-      }
-    }
+        source.value match {
+          case output: ConnectorOutput[_] =>
+            val machine = connectorMachines(output.connector)
+            machine.nexts(output.index) = Some(consumerProvider.asInstanceOf[ConsumerProvider[machine.TT]])
 
-    new RunningStream(this, componentMachines.mapValues(_.running), connectorMachines.mapValues(_.running), graphOps)
+          case component: StreamComponent =>
+            componentMachines(component) match {
+              case machine: StateMachineWithOneOutput[_] =>
+                machine.next = Some(consumerProvider.asInstanceOf[ConsumerProvider[machine.TOut]])
+
+              case _: ConsumerMachine[_, _] | _: DelayedSinkMachine[_, _] => throw new IllegalStateException("Sink machine appears as a Source")
+              case _: ConnectorMachine[_] => throw new IllegalStateException("A ConnectorMachine is in the componentMachines map")
+            }
+        }
+      }
+
+      // Start the initial machines. Note that `allMachines` can contain the same value many times (for Connectors mapped
+      // from multiple StreamComponents which are their edges) so we use .toSet to only `run` each machine once.
+      for (machine <- allMachines.values.toSet if machine.isInstanceOf[RunnableMachine]) {
+        Future {
+          machine.asInstanceOf[RunnableMachine].run()
+        } recover {
+          case NonFatal(e) =>
+            graphOps.failGraph(e)
+        }
+      }
+
+      val stream = new RunningStream(this, componentMachines.mapValues(_.running), connectorMachines.mapValues(_.running), graphOps)
+      runningStreamPromise.trySuccess(stream)
+      stream
+    }
+    catch {
+      case NonFatal(e) =>
+        runningStreamPromise.tryFailure(e)
+        throw e
+    }
   }
 
   /** Validate the current state of the `graph` to make sure it's fully connected, etc. before materializing it. */
